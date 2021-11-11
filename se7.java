5947a7fa41a34aa515078d025f0dc26cc7e7e282
shahithabegum <69446892+shahithabegum@users.noreply.github.com>
2021-11-11 15:39:24 +0530

    Add files via upload

---
+++ se7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+class se7
+{
+  public static void main(String arg[])
+   {
+     try
+         {
+            String s="this is excerise";
+            
+	    System.out.println(s.endsWith("se"));
+	  
+	}
+	catch(Exception e)
+	{
+	  System.out.println(e);
+	}
+       }
+}
